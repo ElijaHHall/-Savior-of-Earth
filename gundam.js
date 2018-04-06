119cedce280202d3edbbbca02a849c1ca1a30822
ElijahHHall <ehall851@gmail.com>
2018-04-06 11:14:34 -0600

    added a start menu and bootstrap

--- gundam.js
+++ gundam.js
@@ -1,4 +1,25 @@
+const gundam = document.getElementById("gundam");
 
+let margValue = 0;
+
+document.addEventListener("keydown", (event) => {
+
+	console.log("Button is pushed");
+});
+
+let bg = document.getElementById("bg");
+
+function fly(e) {
+	if(e.keyCode == 80){
+		margValue += 20;
+		gundam.style.marginLeft = margValue + ("px");
+
+		if(margValue >= 800){
+			alert("The Earth is Saved!");
+		}
+	}
+}
+document.onkeydown = fly;
 
 
 
