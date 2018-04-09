7626494eb0ea3acfd86eb5edf1dbe5280e160a3e
ElijahHHall <ehall851@gmail.com>
2018-04-09 12:27:17 -0600

    gundam moving and add event listeners

---
+++ au.js
@@ -0,0 +1,48 @@
+var falconX = {                                //Mirroring The Screen Position Of "falconX" To Reflect The The Position
+    top : 600,
+    left : 550
+};
+var missiles = [];
+document.onkeydown = function(e) {         //Telling Document To Listen For A Key To Be Preesed Down.
+    console.log(e.keyCode);
+    if (e.keyCode === 37) {                //Listenting For Left Arrow Key
+         console.log("left");
+        falconX.left = falconX.left - 20;         //Move 10 Pexels Back.
+        movefalconX();
+    }
+     else if (e.keyCode === 39 ) {
+        console.log("right");
+        falconX.left = falconX.left + 20;         //Move 10 Pexels.
+        movefalconX();
+    }
+      else if (e.keyCode === 32) {
+        console.log('FiRE');
+        missiles.push({
+            left: falconX.left + 15,
+            top: falconX.top
+        });
+        createMissiles();
+    }
+};
+function movefalconX() {                                                //Creating A Function To Move FalconX
+    document.getElementById('falconX').style.left = falconX.left + "px";     // Grabbing The Element By Its Id And Moving The Style Of Falcon X & Adding Pixels
+}
+function createMissiles() {
+    document.getElementById('missiles').innerHTML = "";
+    for( var missile = 0; missile < missiles.length; missile = missile + 1 ){
+        document.getElementById('missiles').innerHTML +=
+        `<div class='missile' style='left:${missiles[missile].left}px; top:$
+        {missiles[missile].top}px;'></div>`;
+    }
+}
+function moveMissiles() {
+    for( var missile = 0; missile < missiles.length; missile = missile + 1 ){
+        missiles[missile].top = missiles[missile].top  -5;
+    }
+}
+function gameLoop(){
+    setTimeout(gameLoop , 100);
+    moveMissiles();
+    createMissiles();
+}
+    gameLoop();
--- gundam.js
+++ gundam.js
@@ -1,32 +1,71 @@
-const gundam = document.getElementById("gundam");
 
-let margValue = 0;
+// TweenMax.to();
+$(document).ready(function(e) {
 
-document.addEventListener("keydown", (event) => {
 
-	console.log("Button is pushed");
-});
 
-let bg = document.getElementById("bg");
 
-function fly(e) {
-	if(e.keyCode == 80){
-		margValue += 20;
-		gundam.style.marginLeft = margValue + ("px");
+var gundam = {
+	top: 100,
+	left: 100
+};
 
-		if(margValue >= 800){
-			alert("The Earth is Saved!");
+var blast = [];
+
+document.onkeydown = function(e) {
+	console.log(e.keyCode);
+	if (e.keyCode === 65) {
+		console.log("left");
+		gundam.left = gundam.left - 20;
+		movegundam();
+	}
+	else if (e.keyCode === 68) {
+		console.log("right");
+		gundam.left = gundam.left + 20;
+		movegundam();
+	}
+	else if (e.keyCode === 83) {
+		console.log("down");
+		gundam.top = gundam.top  + 20;
+		movegundam();
+	}
+	else if (e.keyCode === 87) {
+		console.log("up");
+		gundam.top = gundam.top  - 20;
+		movegundam();
+	}
+	else if (e.keyCode === 16) {
+		console.log("fire");
+		blast.push({
+			left: gundam.left + 15,
+			top: gundam.top
+		});
+		createBlast();
+	}
+
+	TweenMax.to(".blast", 20, {left: + 800});
+	document.onkeypress = function(e) {
+
+	}
+};
+
+function movegundam() {
+	document.getElementById("mobilesuit").style.left = gundam.left + "px";
+		document.getElementById("mobilesuit").style.top = gundam.top + "px";
+
+}
+function createBlast() {
+	document.getElementById("energyblast").innerHTML = "";
+	for( var blast = 0; blast < blast.length; blast = blast + 1){
+		document.getElementById("blast").innerHTML +=
+		`<div class="blast" style='left:${blast[blast].left}px; top:$
+		{blast[blast].top}px;'></div>`;
+	}
+	function movegundam () {
+		for ( var blast = 0; blast.length; blast = blast + 1){
+			blast[blast].top = blast[blast].top +5;
 		}
 	}
 }
-document.onkeydown = fly;
 
-
-
-
-
-
-
-
-
-
+});
